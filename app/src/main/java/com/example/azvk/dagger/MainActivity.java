8f8863ec57a769e9c8859eb5166b9d699309778f
AlexRedchets <alexey.redchets@gmail.com>
2016-08-17 17:58:12 -0400

    Dagger

--- app/src/main/java/com/example/azvk/dagger/MainActivity.java
+++ app/src/main/java/com/example/azvk/dagger/MainActivity.java
@@ -7,17 +7,22 @@ import android.view.View;
 
 import javax.inject.Inject;
 
+import io.realm.Realm;
+
 public class MainActivity extends AppCompatActivity {
 
-    /*@Inject
+    @Inject
     SharedPreferences sharedPreferences;
-*/
+    @Inject
+    Realm realm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //((App)getApplication()).getNetComponent().inject(this);
+        ((App)getApplication()).getNetComponent().inject(this);
+        ((App)getApplication()).getDbComponent().inject(this);
     }
 
     public void onClick(View vie){
